a1c49a7dbf2b50b09d96a0f9aca6ab0c6797adc8
Gareth Jones <gareth@garethpaul.com>
2016-01-03 20:27:38 -0800

    adding static libs

--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,40 @@
+
 $(document).ready(function(){
-	
+	NProgress.start();
 	Page.init();
 	if (query0){
 		$("#query0").val(query0);
 		Page.search();
 	}
-		
+
+});
+
+$(window).load(function(){
+   NProgress.done();
 });
 
 
 var Page = {
-		
+
 	maxQueries : 4,
 
 	init : function(query) {
 
 		$("#msg_error").hide();
 		$("#msg_success").hide();
-		
+
 		$(".loading").hide();
-		
+
 		$('.datetimepicker').datetimepicker({
 	    	format: 'YYYY-MM-DD HH:mm',
 	    	pickTime: true
 		});
-		
+
 		Page.clear();
 
 		$(".query-input").on("keydown", function (e) {
 
-			// on TAB, add query input 
+			// on TAB, add query input
 			if (e.which == 9) {
 				  Page.queryAdd();
 				  console.log(".query-input keydown" + e.which);
@@ -37,52 +42,52 @@ var Page = {
 
 			return true;
 		});
-		
+
 		$(".query-input").on("keyup", function (e) {
-			
+
 			  // on ENTER, search
 			  if (e.which == 13) {
 				  Page.search();
 			  }
-			  
+
 			  var query = $("#query0").val();
 			  if (!query || query == ''){
 				  Page.clear();
 				  $('#buffer').collapse('show');
 			  }
-			  
+
 			  console.log(".query-input keyup" + e.which);
-			  
+
 			  return true;
-			  
+
 			});
-		
+
 		$(".query-add").hide();
-		
+
 		$("#queryAdd").on("click", function(){
-			
+
 			Page.queryAdd();
 		});
-		
+
 		$(".queryRemove").on("click", function(){
-			
+
 			Page.queryRemove(this);
 
 		});
-		
+
 		$("#scrolltop").on("click", function(){
 
 			window.scrollTo(0, 0);
 			return false;
-			
+
 		});
-		
+
 		$("#search").on("click", function(){
 
 			Page.search();
-			
+
 		});
-		
+
 		$('#media').on("click", function(){
 			Page.toggleTerm("(has:media)", $(this).is(':checked'), false);
 		});
@@ -100,15 +105,15 @@ var Page = {
 				$("#retweet_help").fadeOut();
 			}
 		});
-		
+
 		$(document).on("click", ".term", function(){
 			var val = $(this).val();
 			var newTerm = "(" + val + ")";
 			Page.toggleTerm(newTerm, $(this).is(':checked'), true);
 		});
-		
+
 	},
-	
+
 	toggleTerm : function(term, checked, addIfEmpty){
 		$(".query-input").each(function(){
 			var query = $(this).val();
@@ -127,57 +132,58 @@ var Page = {
 			$(this).val(finalTerm);
 		});
 	},
-	
+
 	clear : function(){
 		$("#activity_volume").hide();
 		$("#activity_tweets").hide();
 		$("#terms_frequency").hide();
 		$("#chart").html("");
 		$("#tweets").html("");
-		
+
 		var collapseOptions = $("#collapseOptions.in");
 		console.log('collapseOptions: ' + collapseOptions.length);
-		
+
 		if (collapseOptions.length > 0){
 			console.log(collapseOptions);
 			$("#collapseOptions").collapse('hide');
 		}
 
 	},
-	
+
 	queryAdd : function(){
 		for (var i = 1; i < Page.maxQueries; i++){
-			var id = "#query" + i + "_holder";  
+			var id = "#query" + i + "_holder";
 			if ($(id).is(':hidden')){
 				$(id).fadeIn();
 				break;
 			}
 		}
 	},
-	
+
 	queryRemove : function(element){
 		var id = $(element).data("id");
 		var id = "#" + id;
 		$(id).hide();
 		$(id).find("input").val("");
 	},
-	
+
 	search : function(){
-		
+
 		Page.hideError();
-		
+		NProgress.start();
+
 		var singleQuery = true;
 		var query0 = $("#query0").val();
 		if (query0){
-			
+			NProgress.inc();
 			var now = "";
 			var start = $("#start").val();
 			var end = $("#end").val();
 			var embedCount = $("#embedCount").val();
-			
+
 			Page.clear();
 			$('#buffer').collapse('hide');
-			
+
 			var queries = [query0];
 			for (var i = 1; i < Page.maxQueries; i++){
 				var query = $("#query" + i).val();
@@ -195,19 +201,22 @@ var Page = {
 			var queryMashup = "(" + query0 + ")";
 			for (var i = 1; i < queries.length; i++){
 				if (queries[i]){
-					queryMashup = queryMashup + " OR (" + queries[i] + ")" 
+					queryMashup = queryMashup + " OR (" + queries[i] + ")"
 				}
 			}
-			
+
 			if ($("#results_frequency").is(':checked')){
+				NProgress.inc();
 				Page.loadFrequency(queryMashup, start, end);
 			}
-			
+
 			if ($("#results_tweets").is(':checked')){
+				NProgress.inc();
 				Page.loadTweets(queryMashup, start, end, embedCount);
 			}
 
 			if ($("#results_export").is(':checked')){
+				NProgress.inc();
 				Page.loadExport(queryMashup, start, end, embedCount);
 			}
 }
@@ -219,21 +228,22 @@ var Page = {
 
 	 	 var data = {"start": start, "end": end, "interval": interval};
 	 	 if (queries.length == 1){
-	 		 data["query"] = queries[0]; 
+	 		 data["query"] = queries[0];
 	 	 } else {
 	 		data["queries"] = queries;
 	 	 }
-		 
+
+		 NProgress.inc();
 		 $.ajax({
 				type : "GET",
 				url : "/query/chart",
 				data : data,
 				dataType : "json",
 				success : function(response) {
-					
+
 					// console.log(response);
 					var start = response.start;
-					
+					NProgress.inc();
 					var args = {
 						    bindto: '#chart',
 						    point: {
@@ -258,11 +268,11 @@ var Page = {
 						            	culling: true,
 						            	count: response.days + 1,
 						            	format: function (x) {
-						            		
+
 						            		var iv = x;
 						            		if (interval == "hour"){
 						            			iv = x / 24;
-						            		} 
+						            		}
 
 						            		var date = new Date(start.valueOf());
 						            	    date.setDate(date.getDate() + iv);
@@ -276,151 +286,154 @@ var Page = {
 						        }
 						    }
 						};
-					
+
 					// console.log(args);
 					var chart = c3.generate(args);
-					
+
 					$("#total").html(Utils.integerFormat(response.total));
 					$("#activity_volume").fadeIn();
 					$("#chart_loading").hide();
-					
+					NProgress.done();
+
 				},
 				error : function(xhr, errorType, exception) {
 					Page.handleError(xhr, errorType, exception);
 				}
 			});
-	
+
 	},
-	
+
 	loadFrequency : function(query, start, end) {
 
 		$("#terms_frequency").hide();
 		$("#frequency_loading").show();
-		
+
 	 	var data = {"start": start, "end": end, "query": query};
-	 	 
+
 	 	// console.log(data);
-		 
+		NProgress.inc();
 		$("#frequency").find("tr:gt(0)").remove();
 
 		templateLoading = $("#templateFrequencyLoading").html();
 		$("#frequency").append(templateLoading);
-			
+
 		 $.ajax({
 				type : "GET",
 				url : "/query/frequency",
 				data : data,
 				dataType : "json",
 				success : function(response) {
-					
+
 					$("#frequency").find("tr:gt(0)").remove();
-					
+
 					var frequency = response.frequency;
 					// console.log(frequency)
 					if (frequency){
-						
+
 						template = $("#templateFrequency").html();
 						Mustache.parse(template);
-						
+
 						for (var i = 0; i < frequency.length; i++){
-							
+
 							var f = frequency[i];
 							f.mentionPercent = function(){
-								return Math.round(this[1] * 100) + "%" 
+								return Math.round(this[1] * 100) + "%"
 							}
 							f.activityPercent = function(){
-								return Math.round(this[3] * 100) + "%" 
+								return Math.round(this[3] * 100) + "%"
 							}
-							
+
 							var output = Mustache.render(template, f);
 							$("#frequency").append(output);
 
 						}
 
 						$(".frequency_sample").html(response.sample);
-						
+
 						$("#terms_frequency").show();
 						$("#frequency_loading").hide();
+						NProgress.done();
 
 					}
-					
+
 				},
 				error : function(xhr, errorType, exception) {
 					Page.handleError(xhr, errorType, exception);
 				}
 			});
-	
+
 	},
-	
+
 	loadTweets : function(query, start, end, embedCount) {
-		
+		 NProgress.inc();
 		 $("#tweets_loading").show();
 		 var video = $("#video").is(':checked');
-		
+
 		 $.ajax({
 				type : "GET",
 				url : "/query/tweets",
 				data : {"query" : query, "start": start, "end": end, "embedCount": embedCount},
 				dataType : "json",
 				success : function(response) {
-					
+
 //					console.log(response);
-					
+
 					template = $("#templateTweet").html();
 					Mustache.parse(template);
-					
+
 					tweets = response.tweets;
 					for (var i = 0; i < tweets.length; i++){
-						
+
 						var tweet = tweets[i];
 						tweet["class"] = "twitter-tweet";
-						
+
 						// uncomment for video tweets
 //						if (video){
 //							tweet["class"] = "twitter-video";
 //						} else {
 //							tweet["class"] = "twitter-tweet";
 //						}
-						
+
 						var output = Mustache.render(template, tweet);
 						$("#tweets").append(output);
 
 					}
-					
+
 					window.twttr.widgets.load()
-					
+
 					$("#activity_tweets").fadeIn();
-					
+
 					$("#tweets_loading").hide();
-														
+					NProgress.done();
+
 				},
 				error : function(xhr, errorType, exception) {
 					Page.handleError(xhr, errorType, exception);
 				}
 			});
-		 
+
 	},
-	
+
 	loadExport : function(query, start, end, embedCount) {
-		
+
 		data = {"query" : query, "start": start, "end": end, "embedCount": embedCount, "export": "csv"};
-		url = "/query/tweets?" + Utils.qs(data); 
-			
+		url = "/query/tweets?" + Utils.qs(data);
+
 		window.open(url);
-		
+
 	},
-	
+
 	showError : function (msg, small){
-		
+
 		template = $("#temlateMessageError").html();
 		Mustache.parse(template);
-		
+
 		var body = {"message": msg}
 		if (small){
-			body['helper'] = "Original query: '"+small+"'"  
+			body['helper'] = "Original query: '"+small+"'"
 		}
 		var output = Mustache.render(template, body);
-	
+
 		$("div.container div.main").prepend(output);
 		$(".loading").hide();
 
@@ -433,16 +446,16 @@ var Page = {
 	},
 
 	handleError : function(xhr, errorType, exception){
-		
+
 		console.log('Error occured');
 		console.log(xhr.responseJSON.error);
 		console.log(xhr.responseJSON.payload);
-		
+
 		var helper = "";
 		if (xhr.responseJSON.payload && xhr.responseJSON.payload.query){
 			helper = xhr.responseJSON.payload.query;
 		}
-		
+
 		Page.showError(xhr.responseJSON.error, helper);
 	},
 
@@ -453,7 +466,7 @@ Utils = {
 	integerFormat : function(x) {
 		    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	},
-		
+
 	qs: function(obj) {
       var str = [];
       for(var p in obj){
@@ -461,7 +474,7 @@ Utils = {
       }
       return str.join("&");
     },
-    
+
     htmlEncode: function(textContent){
     	return $("<textarea/>").text(textContent).html();
     },
